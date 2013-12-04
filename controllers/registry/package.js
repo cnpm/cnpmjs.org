2226976615df222ca2cd48bc3b77ef4f3bee77e2
fengmk2 <fengmk2@gmail.com>
2013-12-04 10:11:08 +0800

    adjust project dir, separate registry and web server

---
+++ controllers/registry/package.js
@@ -0,0 +1,21 @@
+/*!
+ * cnpmjs.org - controllers/registry/package.js
+ *
+ * Copyright(c) cnpmjs.org and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *  dead_horse <dead_horse@qq.com>
+ *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+exports.upload = function (req, res) {
+  res.statusCode = 201;
+  res.end();
+};
--- worker.js
+++ worker.js
@@ -1,7 +1,12 @@
 /*!
- * cnpmjs.org - worker.js 
- * Copyright(c) 2013 
- * Author: dead_horse <dead_horse@qq.com>
+ * cnpmjs.org - worker.js
+ *
+ * Copyright(c) cnpmjs.org and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *  dead_horse <dead_horse@qq.com>
+ *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
  */
 
 'use strict';
@@ -9,15 +14,22 @@
 /**
  * Module dependencies.
  */
+
 var graceful = require('graceful');
 
 var config = require('./config');
-var server = require('./app');
+var registry = require('./servers/registry');
+var web = require('./servers/web');
 
-server.listen(config.webPort);
+registry.listen(config.registryPort);
+web.listen(config.webPort);
+
+console.log('[%s] [worker:%d] Server started, registry server listen at %d, web listen at %d, cluster: %s',
+  new Date(), process.pid,
+  config.registryPort, config.webPort, config.enableCluster);
 
 graceful({
-  server: [server],
+  server: [registry, web],
   error: function (err, throwErrorCount) {
     if (err.message) {
       err.message += ' (uncaughtException throw ' + throwErrorCount + ' times on pid:' + process.pid + ')';
