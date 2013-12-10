57f05d02fe679c8f665a4d5e9c13da939b86cbad
fengmk2 <fengmk2@gmail.com>
2013-12-10 21:47:28 +0800

    add module_id to tag table. #46
    * make sure module_id exists
    * make sure author is same

---
+++ test/proxy/module.test.js
@@ -0,0 +1,34 @@
+/**!
+ * cnpmjs.org - test/proxy/module.test.js
+ *
+ * Copyright(c) cnpmjs.org and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var mm = require('mm');
+var mysql = require('../../common/mysql');
+var Module = require('../../proxy/module');
+
+describe('proxy/module.test.js', function () {
+  afterEach(mm.restore);
+
+  describe('addTag()', function () {
+    it('should add tag auto add module id', function (done) {
+      Module.addTag('mocha', 'test', '1.15.1', function (err, result) {
+        should.not.exist(err);
+        result.should.have.keys('id', 'gmt_modified', 'module_id');
+        done();
+      });
+    });
+  });
+});
