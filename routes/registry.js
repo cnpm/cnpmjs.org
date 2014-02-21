acfa2e418bdda1e58b0e0111bad69207726e40c4
dead_horse <dead_horse@qq.com>
2014-02-22 01:26:33 +0800

    convert registry/module.js to koa type

--- routes/registry.js
+++ routes/registry.js
@@ -29,16 +29,16 @@ function routes(app) {
 
   //before /:name/:version
   //get all modules, for npm search
-  // app.get('/-/all', mod.listAllModules);
-  // app.get('/-/all/since', mod.listAllModulesSince);
-  // //get all module names, for auto completion
-  // app.get('/-/short', mod.listAllModuleNames);
+  app.get('/-/all', mod.listAllModules);
+  app.get('/-/all/since', mod.listAllModulesSince);
+  //get all module names, for auto completion
+  app.get('/-/short', mod.listAllModuleNames);
 
   // module
   app.get('/:name', syncByInstall, mod.show);
   app.get('/:name/:version', syncByInstall, mod.get);
-  // // try to add module
-  // app.put('/:name', [login, publishable], mod.add);
+  // try to add module
+  app.put('/:name', login, publishable, mod.add);
 
   // sync from source npm
   app.put('/:name/sync', sync.sync);
@@ -48,16 +48,16 @@ function routes(app) {
 
   // put tarball
   // https://registry.npmjs.org/cnpmjs.org/-/cnpmjs.org-0.0.0.tgz/-rev/1-c85bc65e8d2470cc4d82b8f40da65b8e
-  app.put('/:name/-/:filename/-rev/:rev', [login, publishable], mod.upload);
-  // // delete tarball
-  // app.delete('/:name/download/:filename/-rev/:rev', [login, publishable], mod.removeTar);
+  app.put('/:name/-/:filename/-rev/:rev', login, publishable, mod.upload);
+  // delete tarball
+  app.delete('/:name/download/:filename/-rev/:rev', login, publishable, mod.removeTar);
 
-  // // put package.json to module
-  // app.put('/:name/:version/-tag/latest', [login, publishable], mod.updateLatest);
+  // put package.json to module
+  app.put('/:name/:version/-tag/latest', login, publishable, mod.updateLatest);
 
-  // // update module, unpublish will PUT this
-  // app.put('/:name/-rev/:rev', [login, publishable], mod.removeWithVersions);
-  // app.delete('/:name/-rev/:rev', [login, publishable], mod.removeAll);
+  // update module, unpublish will PUT this
+  app.put('/:name/-rev/:rev', login, publishable, mod.removeWithVersions);
+  app.delete('/:name/-rev/:rev', login, publishable, mod.removeAll);
 
   // try to create a new user
   // https://registry.npmjs.org/-/user/org.couchdb.user:fengmk2
