c8a7343d45811b126553de67e5b6fab02a867918
fengmk2 <fengmk2@gmail.com>
2014-12-23 01:40:11 +0800

    use __all__ for full downloads

--- services/download_total.js
+++ services/download_total.js
@@ -14,7 +14,6 @@
  * Module dependencies.
  */
 
-var moment = require('moment');
 var DownloadTotal = require('../models').DownloadTotal;
 
 exports.getModuleTotal = function* (name, start, end) {
@@ -34,7 +33,26 @@ exports.getModuleTotal = function* (name, start, end) {
 
 exports.plusModuleTotal = function* (data) {
   var yearMonth = parseYearMonth(data.date);
+  // all module download total
   var row = yield DownloadTotal.find({
+    where: {
+      name: '__all__',
+      date: yearMonth
+    }
+  });
+  if (!row) {
+    row = DownloadTotal.build({
+      name: '__all__',
+      date: yearMonth,
+    });
+  }
+  var field = 'd' + data.date.substring(8, 10);
+  row[field] += data.count;
+  if (row.isDirty) {
+    yield row.save();
+  }
+
+  row = yield DownloadTotal.find({
     where: {
       name: data.name,
       date: yearMonth,
@@ -55,61 +73,7 @@ exports.plusModuleTotal = function* (data) {
 };
 
 exports.getTotal = function* (start, end) {
-  var startMonth = parseYearMonth(start);
-  var endMonth = parseYearMonth(end);
-  var rows = yield DownloadTotal.findAll({
-    where: {
-      date: {
-        gte: startMonth,
-        lte: endMonth
-      },
-    }
-  });
-  var map = {};
-  var downloads = [];
-  rows.forEach(function (row) {
-    var date = String(row.date);
-    var month = date.substring(4, 6);
-    var year = date.substring(0, 4);
-    var yearMonth = year + '-' + month;
-    var days = MONTHS[month];
-    if (month === '02' && moment([Number(year)]).isLeapYear()) {
-      days += 1;
-    }
-    for (var i = 1; i <= days; i++) {
-      var day = i < 10 ? '0' + i : String(i);
-      var field = 'd' + day;
-      var d = yearMonth + '-' + day;
-      var count = row[field];
-      if (d >= start && d <= end && count > 0) {
-        var item = map[d];
-        if (!item) {
-          map[d] = item = {
-            count: 0,
-            date: d
-          };
-          downloads.push(item);
-        }
-        item.count += row[field];
-      }
-    }
-  });
-  return downloads;
-};
-
-var MONTHS = {
-  '01': 31,
-  '02': 28, // leap year: 29
-  '03': 31,
-  '04': 30,
-  '05': 31,
-  '06': 30,
-  '07': 31,
-  '08': 31,
-  '09': 30,
-  '10': 31,
-  '11': 30,
-  '12': 31
+  return yield* exports.getModuleTotal('__all__', start, end);
 };
 
 function parseYearMonth(date) {
@@ -123,18 +87,15 @@ function formatRows(rows, startDate, endDate) {
     var month = date.substring(4, 6);
     var year = date.substring(0, 4);
     var yearMonth = year + '-' + month;
-    var days = MONTHS[month];
-    if (month === '02' && moment([Number(year)]).isLeapYear()) {
-      days += 1;
-    }
-    for (var i = 1; i <= days; i++) {
+    for (var i = 1; i <= 31; i++) {
       var day = i < 10 ? '0' + i : String(i);
       var field = 'd' + day;
       var d = yearMonth + '-' + day;
-      if (d >= startDate && d <= endDate) {
+      var count = row[field];
+      if (count > 0 && d >= startDate && d <= endDate) {
         dates.push({
           name: row.name,
-          count: row[field],
+          count: count,
           date: d
         });
       }
