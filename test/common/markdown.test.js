603fdeea06e6d4a338b5a1ae0c41baf55601a5ba
fengmk2 <fengmk2@gmail.com>
2015-01-03 02:22:27 +0800

    fix(markdown): use marked instead of remarkable
    Fixes #552

---
+++ test/common/markdown.test.js
@@ -0,0 +1,29 @@
+/**!
+ * cnpmjs.org - test/common/markdown.test.js
+ *
+ * Copyright(c) fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var path = require('path');
+var fs = require('fs');
+var markdown = require('../../common/markdown');
+
+describe('common/markdown.test.js', function () {
+  var fixtures = path.join(__dirname, '..', 'fixtures');
+
+  it('should render sonido readme', function () {
+    var readme = fs.readFileSync(path.join(fixtures, 'sonido.md'), 'utf8');
+    var md = markdown.render(readme);
+    md.should.equal('<p>Configuration Wizard: &lt;!--- This is what the user will see during the configuration -&gt;</p>\n');
+  });
+});
---
+++ test/fixtures/sonido.md
@@ -0,0 +1,1 @@
+Configuration Wizard: <!--- This is what the user will see during the configuration ->
