e1876a5503de546cf847d2371dbff42048db4519
ibigbug <i@xiaoba.me>
2015-12-06 19:48:07 +0800

    use async render.

--- controllers/web/admin/index.js
+++ controllers/web/admin/index.js
@@ -7,5 +7,5 @@ exports.user = require('./user');
 
 exports.admin = function* () {
   this.set('Content-Type', 'text/html; charset=utf-8');
-  this.body = fs.readFileSync(path.join(path.dirname(__dirname), '../../view/web/admin', 'index.html'));
+  this.body = fs.createReadStream(path.join(path.dirname(__dirname), '../../view/web/admin', 'index.html'));
 };
