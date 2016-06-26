5fb346e1d309b1ae342cc1639f41d09d6797f145
fengmk2 <m@fengmk2.com>
2016-06-26 13:17:30 +0800

    fix: logger seperator should be one EOL

--- common/logger.js
+++ common/logger.js
@@ -1,38 +1,40 @@
 'use strict';
 
-var debug = require('debug')('cnpmjs.org:logger');
-var formater = require('error-formater');
-var Logger = require('mini-logger');
-var utility = require('utility');
-var util = require('util');
-var config = require('../config');
-var mail = require('./mail');
+const debug = require('debug')('cnpmjs.org:logger');
+const formater = require('error-formater');
+const Logger = require('mini-logger');
+const utility = require('utility');
+const util = require('util');
+const os = require('os');
+const config = require('../config');
+const mail = require('./mail');
 
-var isTEST = process.env.NODE_ENV === 'test';
-var categories = ['sync_info', 'sync_error'];
+const isTEST = process.env.NODE_ENV === 'test';
+const categories = ['sync_info', 'sync_error'];
 
-var logger = module.exports = Logger({
+const logger = module.exports = Logger({
   categories: categories,
   dir: config.logdir,
   duration: '1d',
   format: '[{category}.]YYYY-MM-DD[.log]',
   stdout: config.debug && !isTEST,
-  errorFormater: errorFormater
+  errorFormater: errorFormater,
+  seperator: os.EOL,
 });
 
-var to = [];
+const to = [];
 for (var user in config.admins) {
   to.push(config.admins[user]);
 }
 
 function errorFormater(err) {
-  var msg = formater.both(err);
+  const msg = formater.both(err);
   mail.error(to, msg.json.name, msg.text);
   return msg.text;
 }
 
 logger.syncInfo = function () {
-  var args = [].slice.call(arguments);
+  const args = [].slice.call(arguments);
   if (typeof args[0] === 'string') {
     args[0] = util.format('[%s][%s] ', utility.logDate(), process.pid) + args[0];
   }
@@ -43,7 +45,7 @@ logger.syncInfo = function () {
 };
 
 logger.syncError =function () {
-  var args = [].slice.call(arguments);
+  const args = [].slice.call(arguments);
   if (typeof args[0] === 'string') {
     args[0] = util.format('[%s][%s] ', utility.logDate(), process.pid) + args[0];
   }
