ea3beceef7ac4a4ca4ee89c97854d7fd1d366c18
fengmk2 <m@fengmk2.com>
2016-07-26 18:32:33 +0800

    feat: enable maxrequests middleware (#1003)

--- servers/registry.js
+++ servers/registry.js
@@ -1,19 +1,4 @@
-/**!
- * cnpmjs.org - servers/registry.js
- *
- * Copyright(c) cnpmjs.org and other contributors.
- * MIT Licensed
- *
- * Authors:
- *  dead_horse <dead_horse@qq.com>
- *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
-
-"use strict";
-
-/**
- * Module dependencies.
- */
+'use strict';
 
 var koa = require('koa');
 var app = module.exports = koa();
@@ -28,25 +13,27 @@ var staticCache = require('../middleware/static');
 var notFound = require('../middleware/registry_not_found');
 var cors = require('kcors');
 var proxyToNpm = require('../middleware/proxy_to_npm');
+var maxrequests = require('koa-maxrequests');
 
+app.use(maxrequests());
 app.use(block());
 middlewares.jsonp(app);
-app.use(middlewares.rt({headerName: 'X-ReadTime'}));
+app.use(middlewares.rt({ headerName: 'X-ReadTime' }));
 app.use(middlewares.rewrite('/favicon.ico', '/favicon.png'));
 app.use(staticCache);
 
 app.keys = ['todokey', config.sessionSecret];
 app.proxy = true;
-app.use(middlewares.bodyParser({jsonLimit: config.jsonLimit}));
+app.use(middlewares.bodyParser({ jsonLimit: config.jsonLimit }));
 app.use(cors({
-  allowMethods: 'GET,HEAD'
+  allowMethods: 'GET,HEAD',
 }));
 app.use(auth());
 app.use(proxyToNpm());
 app.use(notFound);
 
 if (config.enableCompress) {
-  app.use(middlewares.compress({threshold: 150}));
+  app.use(middlewares.compress({ threshold: 150 }));
 }
 app.use(middlewares.conditional());
 app.use(middlewares.etag());
