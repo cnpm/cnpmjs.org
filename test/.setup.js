889a9c7ee50e1f14bbdfcc8851d403828643b2e8
fengmk2 <fengmk2@gmail.com>
2017-02-09 20:57:15 +0800

    test: use npm scripts

--- test/.setup.js
+++ test/.setup.js
@@ -3,6 +3,13 @@
 const runScript = require('runscript');
 const initDb = require.resolve('./init_db');
 
-before(() => {
-  return runScript(`node ${initDb}`);
+before(function* () {
+  if (process.env.DB === 'mysql') {
+    yield runScript('mysql -uroot -e \'DROP DATABASE IF EXISTS cnpmjs_test;\'');
+    yield runScript('mysql -uroot -e \'CREATE DATABASE cnpmjs_test;\'');
+  } else if (process.env.DB === 'postgres') {
+    yield runScript('psql -c \'DROP DATABASE IF EXISTS cnpmjs_test;\' -U postgres');
+    yield runScript('psql -c \'CREATE DATABASE cnpmjs_test;\' -U postgres');
+  }
+  yield runScript(`node ${initDb}`);
 });
