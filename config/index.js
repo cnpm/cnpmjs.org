f34e66780a752b0fb32e6059ea5021c384681b9c
fengmk2 <fengmk2@gmail.com>
2017-05-18 14:56:11 +0800

    feat: add globalHook on config
    Remote hooks need to wait for
    https://github.com/cnpm/cnpmjs.org/issues/624

--- config/index.js
+++ config/index.js
@@ -225,6 +225,10 @@ var config = {
   // https://github.com/cnpm/cnpmjs.org/issues/1149
   // if enable this option, must create module_abbreviated and package_readme table in database
   enableAbbreviatedMetadata: false,
+
+  // global hook function: function* (envelope) {}
+  // envelope format please see https://github.com/npm/registry/blob/master/docs/hooks/hooks-payload.md#payload
+  globalHook: null,
 };
 
 if (process.env.NODE_ENV === 'test') {
