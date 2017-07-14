5b63dbe514ebf43b1720212dfcdb1bec8556bcf0
fengmk2 <fengmk2@gmail.com>
2017-07-14 11:10:56 +0800

    feat: list all private packages names

--- controllers/registry/package/list_shorts.js
+++ controllers/registry/package/list_shorts.js
@@ -1,21 +1,32 @@
-/**!
- * Copyright(c) cnpm and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.com)
- */
-
 'use strict';
 
-/**
- * Module dependencies.
- */
-
-var packageService = require('../../../services/package');
+const packageService = require('../../../services/package');
+const config = require('../../../config');
 
 // GET /-/short
-// List all packages names only
+// List public all packages names only
 module.exports = function* () {
+  if (this.query.private_only) {
+    const tasks = [];
+    for (let i = 0; i < config.scopes.length; i++) {
+      const scope = config.scopes[i];
+      tasks.push(packageService.listPrivateModulesByScope(scope));
+    }
+
+    if (config.privatePackages && config.privatePackages.length > 0) {
+      tasks.push(packageService.listModules(config.privatePackages));
+    }
+
+    const results = yield tasks;
+    const names = [];
+    for (const rows of results) {
+      for (const row of rows) {
+        names.push(row.name);
+      }
+    }
+    this.body = names;
+    return;
+  }
+
   this.body = yield packageService.listAllPublicModuleNames();
 };
--- test/controllers/registry/package/list_shorts.test.js
+++ test/controllers/registry/package/list_shorts.test.js
@@ -1,43 +1,62 @@
-/**!
- * cnpmjs.org - test/controllers/registry/package/list_shorts.test.js
- *
- * Copyright(c) cnpmjs.org and other contributors.
- * MIT Licensed
- *
- * Authors:
- *  fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
-
 'use strict';
 
-/**
- * Module dependencies.
- */
+const pedding = require('pedding');
+const should = require('should');
+const request = require('supertest');
+const mm = require('mm');
+const config = require('../../../../config');
+const app = require('../../../../servers/registry');
+const utils = require('../../../utils');
 
-var should = require('should');
-var request = require('supertest');
-var mm = require('mm');
-var config = require('../../../../config');
-var app = require('../../../../servers/registry');
-var utils = require('../../../utils');
-
-describe('controllers/registry/package/list_shorts.test.js', function () {
+describe('test/controllers/registry/package/list_shorts.test.js', () => {
   afterEach(mm.restore);
 
-  before(function (done) {
+  before(done => {
+    done = pedding(2, done);
+    const pkg = utils.getPackage('@cnpmtest/testmodule-list-shorts1', '1.0.0', utils.admin);
+    pkg.versions['1.0.0'].dependencies = {
+      '@cnpmtest/testmodule-list-shorts2': '~1.0.0',
+    };
+    request(app.listen())
+    .put('/' + pkg.name)
+    .set('authorization', utils.adminAuth)
+    .send(pkg)
+    .expect(201, done);
+
+    const pkg2 = utils.getPackage('@cnpmtest/testmodule-list-shorts2', '1.0.0', utils.admin);
+    request(app.listen())
+    .put('/' + pkg2.name)
+    .set('authorization', utils.adminAuth)
+    .send(pkg2)
+    .expect(201, done);
+  });
+
+  before(done => {
     mm(config, 'syncModel', 'all');
     utils.sync('pedding', done);
   });
 
-  describe('GET /-/short', function () {
-    it('should get 200', function (done) {
+  describe('GET /-/short', () => {
+    it('should get 200', done => {
       request(app)
       .get('/-/short')
-      .expect(200, function (err, res) {
+      .expect(200, (err, res) => {
         should.not.exist(err);
         res.body.should.be.an.Array;
         res.body.length.should.above(0);
         res.body.indexOf('pedding').should.above(-1);
+        done();
+      });
+    });
+
+    it('should list private packages only', done => {
+      request(app)
+      .get('/-/short?private_only=true')
+      .expect(200, (err, res) => {
+        should.not.exist(err);
+        res.body.should.be.an.Array;
+        res.body.length.should.above(0);
+        res.body.indexOf('@cnpmtest/testmodule-list-shorts1').should.above(-1);
         done();
       });
     });
