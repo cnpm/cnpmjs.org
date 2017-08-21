df3c9fa82202dedb147d33ef8474f154f9e7d7f9
fengmk2 <fengmk2@gmail.com>
2017-08-21 14:51:53 +0800

    deps: upgrade markdown-it to 8 (#1214)

--- common/markdown.js
+++ common/markdown.js
@@ -1,18 +1,4 @@
-/**!
- * cnpmjs.org - common/markdown.js
- *
- * Copyright(c) fengmk2 and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
-
 'use strict';
-
-/**
- * Module dependencies.
- */
 
 var xss = require('xss');
 var MarkdownIt = require('markdown-it');
