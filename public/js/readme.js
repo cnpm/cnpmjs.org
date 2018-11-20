8d4082fc17f85a1886e55dc6a13b610a94c7eb28
jpuncle <jack_zhxk@163.com>
2018-11-20 10:16:08 +0800

    fix: Don't display sync info when the sync mode is none

--- public/js/readme.js
+++ public/js/readme.js
@@ -24,26 +24,28 @@ $(function () {
     $('#node-version').html(data.node_version || 'v0.10.22');
     $('#app-version').html(data.app_version || '0.0.0');
 
-    if (data.sync_model === 'all') {
-      $('#sync-model').html('This registry will sync all packages from official registry.');
-      $('#last-sync-time').html(new Date(data.last_sync_time));
-    } else if (data.sync_model === 'exist') {
-      $('#sync-model').html('This registry will only update exist packages from official registry.');
-      $('#last-sync-time').html(new Date(data.last_exist_sync_time));
+    if (data.sync_model !== 'none') {
+      if (data.sync_model === 'all') {
+        $('#sync-model').html('This registry will sync all packages from official registry.');
+        $('#last-sync-time').html(new Date(data.last_sync_time));
+      } else if (data.sync_model === 'exist') {
+        $('#sync-model').html('This registry will only update exist packages from official registry.');
+        $('#last-sync-time').html(new Date(data.last_exist_sync_time));
+      }
+
+      $('#need-sync').html(data.need_sync_num);
+      $('#success-sync').html(data.success_sync_num);
+      $('#fail-sync').html(data.fail_sync_num);
+      $('#left-sync').html(data.left_sync_num);
+      $('#percent-sync').html(Math.floor(data.success_sync_num / data.need_sync_num * 100));
+      $('#last-success-name').html('<a target="_blank" href="/package/' + data.last_sync_module + '">' +
+        data.last_sync_module + '</a>');
+
+      if (!data.sync_status) {
+        $('.syncing').remove();
+      }
+
+      $('.sync').show();
     }
-
-    $('#need-sync').html(data.need_sync_num);
-    $('#success-sync').html(data.success_sync_num);
-    $('#fail-sync').html(data.fail_sync_num);
-    $('#left-sync').html(data.left_sync_num);
-    $('#percent-sync').html(Math.floor(data.success_sync_num / data.need_sync_num * 100));
-    $('#last-success-name').html('<a target="_blank" href="/package/' + data.last_sync_module + '">' +
-      data.last_sync_module + '</a>');
-
-    if (!data.sync_status) {
-      $('.syncing').remove();
-    }
-
-    $('.sync').show();
   });
 });
