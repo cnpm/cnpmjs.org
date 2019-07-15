97c2c14084fc5a81ffc7ad59aa8981c444f4bffa
Mg303030 <52697277+Mg303030@users.noreply.github.com>
2019-07-15 05:47:32 +0300

    Revert "refactor: normalize database structure (#1376)"
    This reverts commit 26d7147562a1ae21db8bfec26983daf311353d96.

--- models/module_log.js
+++ models/module_log.js
@@ -15,15 +15,15 @@
  */
 
 /*
- CREATE TABLE IF NOT EXISTS `module_log` (
+CREATE TABLE IF NOT EXISTS `module_log` (
  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
  `gmt_create` datetime NOT NULL COMMENT 'create time',
  `gmt_modified` datetime NOT NULL COMMENT 'modified time',
- `username` varchar(100) NOT NULL COMMENT 'which username',
- `name` varchar(214) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'module name',
- `log` longtext COMMENT 'the raw log',
+ `username` varchar(100) NOT NULL,
+ `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'module name',
+ `log` longtext,
  PRIMARY KEY (`id`),
- KEY `idx_name` (`name`)
+ KEY `module_log_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='module sync log';
  */
 
@@ -35,20 +35,19 @@ module.exports = function (sequelize, DataTypes) {
       comment: 'user name'
     },
     name: {
-      type: DataTypes.STRING(214),
+      type: DataTypes.STRING(100),
       allowNull: false,
       comment: 'module name',
     },
     log: {
-      type: DataTypes.LONGTEXT,
-      comment: 'the raw log',
+      type: DataTypes.LONGTEXT
     }
   }, {
     tableName: 'module_log',
     comment: 'module sync log',
     indexes: [
       {
-        fields: ['name'],
+        fields: ['name']
       }
     ],
     classMethods: {
