62f58a6c1c0bb48ef8f78e0cc28e6242f63aff63
cindyliu-tec <56294517+cindyliu-tec@users.noreply.github.com>
2019-11-01 15:19:07 +0800

    使用npm access ls-packages命令报404
    npm access ls-packages命令访问路由格式为http://registryhost/-/user/name/package?format=cli，原代码路由多了两个s，导致404

--- routes/registry.js
+++ routes/registry.js
@@ -101,7 +101,7 @@ function routes(app) {
 
   // list all packages of user
   app.get('/-/by-user/:user', userPackage.list);
-  app.get('/-/users/:user/packages', listPackagesByUser);
+  app.get('/-/user/:user/package', listPackagesByUser);
 
   // download times
   app.get('/downloads/range/:range/:name', downloadTotal);
