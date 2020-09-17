54003752968278f119bad7ae5658f57d26f5adb5
killagu <killa123@126.com>
2020-09-17 11:14:04 +0800

    chore: token to unique index, new user index

--- docs/db.sql
+++ docs/db.sql
@@ -323,11 +323,12 @@ CREATE TABLE IF NOT EXISTS `token` (
  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
  `gmt_create` datetime NOT NULL COMMENT 'create time',
  `gmt_modified` datetime NOT NULL COMMENT 'modified time',
- `token` varchar(200) NOT NULL COMMENT 'token',
+ `token` varchar(100) NOT NULL COMMENT 'token',
  `user` varchar(100) NOT NULL COMMENT 'user name',
- `readonly` tinyint(1) DEFAULT '0' COMMENT 'readonly or not, 1: true, other: false',
+ `readonly` tinyint NOT NULL DEFAULT 0 COMMENT 'readonly or not, 1: true, other: false',
  `token_key` varchar(200) NOT NULL COMMENT 'token sha512 hash',
  `cidr_whitelist` varchar(500) NOT NULL COMMENT 'ip list, ["127.0.0.1"]',
  PRIMARY KEY (`id`),
- KEY `idx_token` (`token`)
+ UNIQUE KEY `uk_token` (`token`),
+ KEY `idx_user` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='token info';
--- models/token.js
+++ models/token.js
@@ -5,20 +5,21 @@ CREATE TABLE IF NOT EXISTS `token` (
  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
  `gmt_create` datetime NOT NULL COMMENT 'create time',
  `gmt_modified` datetime NOT NULL COMMENT 'modified time',
- `token` varchar(200) NOT NULL COMMENT 'token',
+ `token` varchar(100) NOT NULL COMMENT 'token',
  `user` varchar(100) NOT NULL COMMENT 'user name',
- `readonly` tinyint(1) DEFAULT '0' COMMENT 'readonly or not, 1: true, other: false',
+ `readonly` tinyint NOT NULL DEFAULT 0 COMMENT 'readonly or not, 1: true, other: false',
  `token_key` varchar(200) NOT NULL COMMENT 'token sha512 hash',
  `cidr_whitelist` varchar(500) NOT NULL COMMENT 'ip list, ["127.0.0.1"]',
  PRIMARY KEY (`id`),
- KEY `idx_token` (`token`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='token info';
+ UNIQUE KEY `uk_token` (`token`),
+ KEY `idx_user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='token info';
  */
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Token', {
     token: {
-      type: DataTypes.STRING(256),
+      type: DataTypes.STRING(100),
       allowNull: false,
       comment: 'token',
     },
@@ -50,7 +51,11 @@ module.exports = function(sequelize, DataTypes) {
     comment: 'token info',
     indexes: [
       {
+        unique: true,
         fields: [ 'token' ],
+      },
+      {
+        fields: [ 'user' ],
       }
     ],
     classMethods: {
