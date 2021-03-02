a8e9b5669da21b245b7744554869d42239689956
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2021-03-02 10:49:23 +0800

    fix: model init

--- app/model/user.js
+++ app/model/user.js
@@ -1,23 +1,24 @@
 'use strict';
 
 module.exports = (app, Model) => {
-  const { STRING, TEXT, BIGINT, BOOLEAN } = Model;
+  const { STRING, TEXT, BIGINT, BOOLEAN } = Model.DataTypes;
 
-  return class User extends Model {
-    static get table() { return 'user'; }
-    static get schema() {
-      return {
-        id: { type: BIGINT.UNSIGNED, autoIncrement: true },
-        name: { type: STRING(100), allowNull: false },
-        salt: { type: STRING(100), allowNull: false },
-        passwordSha: { type: STRING(100), allowNull: false },
-        ip: { type: STRING(64), allowNull: false },
-        roles: { type: STRING(200), allowNull: false, defaultValue: '[]' },
-        rev: { type: STRING(40), allowNull: false },
-        email: { type: STRING(400), allowNull: false },
-        json: { type: TEXT, allowNull: true },
-        isNpmUser: { type: BOOLEAN, allowNull: false, defaultValue: false, column: 'npm_user' },
-      };
-    }
-  };
+  class User extends Model {}
+
+  User.init({
+    id: { type: BIGINT.UNSIGNED, autoIncrement: true, primaryKey: true },
+    name: { type: STRING(100), allowNull: false },
+    salt: { type: STRING(100), allowNull: false },
+    passwordSha: { type: STRING(100), allowNull: false },
+    ip: { type: STRING(64), allowNull: false },
+    roles: { type: STRING(200), allowNull: false, defaultValue: '[]' },
+    rev: { type: STRING(40), allowNull: false },
+    email: { type: STRING(400), allowNull: false },
+    json: { type: TEXT, allowNull: true },
+    isNpmUser: { type: BOOLEAN, allowNull: false, defaultValue: false, column: 'npm_user' },
+  }, {
+    table: 'user',
+  });
+
+  return User;
 };
--- test/.setup.js
+++ test/.setup.js
@@ -2,6 +2,10 @@
 
 const { app } = require('egg-mock/bootstrap');
 
+before(async () => {
+  await app.orm.sync();
+});
+
 beforeEach(async () => {
   await app.orm.User.driver.query('TRUNCATE user');
 });
