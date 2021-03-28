eb8d9cb1e2484bb41a001e9944b5ad0ab1314220
fengmk2 <fengmk2@gmail.com>
2021-03-28 16:55:01 +0800

    feat: export models ref to app
    let custom middleware can access app.models

--- models/module.js
+++ models/module.js
@@ -1,18 +1,4 @@
-/**!
- * cnpmjs.org - models/module.js
- *
- * Copyright(c) fengmk2 and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
- */
-
 'use strict';
-
-/**
- * Module dependencies.
- */
 
 /*
 CREATE TABLE IF NOT EXISTS `module` (
