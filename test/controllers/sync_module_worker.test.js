7351556bd4f3a8134f26608dcec7738ab1f6d79c
fengmk2 <fengmk2@gmail.com>
2021-09-27 17:14:06 +0800

    f

--- test/controllers/sync_module_worker.test.js
+++ test/controllers/sync_module_worker.test.js
@@ -166,6 +166,7 @@ describe('test/controllers/sync_module_worker.test.js', () => {
 
     let pkg;
     let pkgV2;
+    let pkgV3;
     function checkResult() {
       return function (done) {
         request(app)
@@ -181,6 +182,10 @@ describe('test/controllers/sync_module_worker.test.js', () => {
           assert(pkgV2.os[0] === 'linux');
           assert(pkgV2.cpu[0] === 'x64');
           assert(pkgV2.peerDependenciesMeta);
+          pkgV3 = res.body.versions['3.0.0'];
+          assert(!pkgV3.os);
+          assert(!pkgV3.cpu);
+          assert(pkgV3.peerDependenciesMeta);
         })
         .expect(200, done);
       };
@@ -203,6 +208,11 @@ describe('test/controllers/sync_module_worker.test.js', () => {
       cpu: undefined,
       peerDependenciesMeta: undefined,
     });
+    yield packageService.updateModuleAbbreviatedPackage({
+      name: pkgV3.name,
+      version: pkgV3.version,
+      peerDependenciesMeta: undefined,
+    });
 
     function checkModifiyResult() {
       return function (done) {
@@ -214,6 +224,7 @@ describe('test/controllers/sync_module_worker.test.js', () => {
           assert(!res.body.versions['1.0.0'].os);
           assert(!res.body.versions['1.0.0'].cpu);
           assert(!res.body.versions['2.0.0'].peerDependenciesMeta);
+          assert(!res.body.versions['3.0.0'].peerDependenciesMeta);
         })
         .expect(200, done);
       };
